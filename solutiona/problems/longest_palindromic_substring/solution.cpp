17f39ba269496e58dc4dabeead512c9ccdc6c9fa
Sayan Mondal <sayan.ee.nitd@gmail.com>
2021-11-07 09:38:08 +0000

    Sync LeetCode submission - Longest Palindromic Substring (cpp)

---
+++ solutiona/problems/longest_palindromic_substring/solution.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+
+	if( s.size()<2 ){
+		return s;
+	}
+
+	int i=0;
+	int l,r;
+	string substr="";
+	int max_len=0;
+
+
+
+	for( i =0 ; i < s.size() ; i++){
+
+		int t_len=0;
+		
+
+		//  check for odd palindromes
+		l = i , r = i;
+		
+		while( l>=0 && r<s.size() && s[l] == s[r] ){
+
+			t_len = r-l+1;
+
+			if( t_len > max_len ){
+				max_len = t_len;
+				substr = s.substr(l,t_len);
+			}
+
+			l--, r++;
+		}
+
+		// check for even palindromes
+
+		l = i , r = i+1;
+		
+		while( l>=0 && r<s.size() && s[l] == s[r] ){
+
+			t_len = r-l+1;
+
+			if( t_len > max_len ){
+				max_len = t_len;
+				substr = s.substr(l,t_len);
+			}
+
+			l--, r++;
+		}
+
+
+
+
+	}
+
+	return substr;
+        
+    }
+};
