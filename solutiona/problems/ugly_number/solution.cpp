09a7cc225c404987e03689868afef841f05bf0bf
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-01-06 19:56:40 +0000

    Sync LeetCode submission - Ugly Number (cpp)

---
+++ solutiona/problems/ugly_number/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    Solution(){
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);        
+    }
+
+    
+    
+    bool isUgly(int n) {
+         
+        if( n == 0 ) return false;
+        
+        while(n%2 == 0 ){
+            n /= 2;
+        }
+
+        
+        while(n%3 == 0){
+            n /= 3;
+        }
+        
+        
+        while(n%5 == 0){
+            n /= 5;
+        }
+        
+        return n == 1 ;
+    }
+};
