af985f5d34ec02a806a2b6cef6870c0c4c09551d
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-01-07 23:18:58 +0000

    Sync LeetCode submission - Ugly Number II (cpp)

--- solutiona/problems/ugly_number_ii/solution.cpp
+++ solutiona/problems/ugly_number_ii/solution.cpp
@@ -5,33 +5,23 @@ public:
         cin.tie(0);
         cout.tie(0);        
     }
-    
-    int nthUglyNumber(int n) {
-        
-        int ugly[n];
-        int i2=0,i3=0,i5=0;
-        int next2=2, next3=3, next5=5;
-        ugly[0]=1;
-        for(int i=1; i<n; i++)
-        {
-            ugly[i] = min(next2, min(next3, next5));
-            if(ugly[i] == next2)
-            {
-                i2++;
-                next2=2*ugly[i2];
-            }
-            if(ugly[i] == next3)
-            {
-                i3++;
-                next3=3*ugly[i3];
-            }
-            if(ugly[i] == next5)
-            {
-                i5++;
-                next5=5*ugly[i5];
-            }
-        }
-        return ugly[n-1];
-    }
 
+    int nthUglyNumber(int n) {        
+        
+        int two = 0 , three = 0 , five = 0 ;
+        vector <int> ugly(n+1,0);
+        ugly[0] = 1;
+        
+        for( int i = 1 ; i <=n ; i++ ){
+            
+            ugly[i] = min({ugly[two]*2 , ugly[three]*3 , ugly[five]*5 });
+            if(ugly[i] == ugly[two]*2 ) two++;
+            if(ugly[i] == ugly[three]*3 ) three++;
+            if(ugly[i] == ugly[five]*5 ) five++;
+            
+        }
+        
+        return ugly[n-1];
+        
+    }
 };
