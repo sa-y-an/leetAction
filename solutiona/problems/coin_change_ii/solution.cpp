7cc92d9c90ac1be766b6a18b10cea16729d92b82
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-01-15 03:09:20 +0000

    Sync LeetCode submission - Coin Change II (cpp)

--- solutiona/problems/coin_change_ii/solution.cpp
+++ solutiona/problems/coin_change_ii/solution.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    Solution(){
-        ios_base::sync_with_stdio(false);
-        cin.tie(0);
-        cout.tie(0);        
-    }
+    int change(int target, vector<int>& nums) {
+    int ways[nums.size()+1][target+1];
+	memset(ways,0, sizeof(ways));
 
-    
-    int change(int total, vector<int>& coins) {
-        
-        vector <int> ways(total+1,0);
-        ways[0] = 1;
-        
-        for ( auto coin : coins ) {     
-            for(int amount = 1 ; amount <= total ; amount++ ){
-                if( amount >= coin ) ways[amount] += ways[amount-coin];
-             }
-        }
-        
-        return ways[total];
+	for( int idx = 0 ; idx <= nums.size() ; idx++ ) ways[idx][0] = 1;
+
+	for( int idx = 1 ; idx <= nums.size() ; idx++ ){
+		for( int total = 1 ; total <= target ; total++ ){
+			if(total >= nums[idx-1])
+				ways[idx][total] = ways[idx][total-nums[idx-1]] + ways[idx-1][total];
+			else 
+				ways[idx][total] = ways[idx-1][total];
+		}
+	}
+
+
+	return ways[nums.size()][target];   
     }
 };
