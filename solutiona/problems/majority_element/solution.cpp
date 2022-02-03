918890887f576770f8a094f57296fae0217fcf84
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-02-03 07:05:10 +0000

    Sync LeetCode submission - Majority Element (cpp)

--- solutiona/problems/majority_element/solution.cpp
+++ solutiona/problems/majority_element/solution.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-    
-    Solution(){
-        ios_base::sync_with_stdio(false);
-        cin.tie(0);
-        cout.tie(0);        
-    }
-
-    
     int majorityElement(vector<int>& nums) {
         
-    int cnt=1,curr=nums[0];
-    for(int i=1;i<nums.size();i++){
-            if(nums[i]==curr)  cnt++;
-            else cnt--;
-            if(cnt==0){
-                
-         curr=nums[i+1];
-          
-       }
-   }
-    return curr;
+        unordered_map <int,int> elements;
 
+        for ( int num : nums ){
+            elements[num]++;
+        }
+
+        int majority = nums.size()/2;
+        cout<<majority<<" ";
+        for( auto it : elements) {
+            cout<<it.first<<" "<<it.second<<"\n";
+            if( it.second > majority) return it.first;
+        }
+
+        return 1;
+        
     }
 };
