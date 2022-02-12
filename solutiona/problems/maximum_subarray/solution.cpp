6d5186efee0de3deb5b7f7d6860f6cf5a611b482
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-02-12 05:29:16 +0000

    Sync LeetCode submission - Maximum Subarray (cpp)

--- solutiona/problems/maximum_subarray/solution.cpp
+++ solutiona/problems/maximum_subarray/solution.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
-    Solution(){
-        ios_base::sync_with_stdio(false);
-        cin.tie(0);
-        cout.tie(0);        
-    }
-
     int maxSubArray(vector<int>& nums) {
-        int ans = INT_MIN;
-        int a = 0;
-        for( int x : nums ){
-            a += x;
-            ans = max(a,ans);
-            a = max(a,0);
+        int sum = 0;
+        int maxSum = INT_MIN;
+        for( auto num:nums){
+            sum += num;
+            maxSum = max(sum, maxSum);
+            sum = max(sum,0);
         }
-        return ans;
+        return maxSum;
     }
 };
