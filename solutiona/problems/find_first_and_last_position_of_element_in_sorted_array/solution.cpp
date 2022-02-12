9bdf32e2fc3a787bb765e36035209a0bcced53e7
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-02-12 05:47:58 +0000

    Sync LeetCode submission - Find First and Last Position of Element in Sorted Array (cpp)

--- solutiona/problems/find_first_and_last_position_of_element_in_sorted_array/solution.cpp
+++ solutiona/problems/find_first_and_last_position_of_element_in_sorted_array/solution.cpp
@@ -1,44 +1,41 @@
 class Solution {
 public:
-    
-
+        
     vector<int> searchRange(vector<int>& nums, int target) {
+        int low = 0 , high = nums.size()-1;
         
-        int first = - 1, last = -1;
-        int start = 0 , end = nums.size()-1;
-        
-        while( start <= end ){
-            
-            int mid = start + ( end-start)/2;
-            
-            if( nums[mid] > target ) end = mid-1;
-            else if( nums[mid] < target ) start = mid+1;
-            else {
+        // first position
+        int first = -1;
+        while( low <= high ){
+            int mid = low + (high-low)/2;
+            if( nums[mid] == target){
                 first = mid;
-                end = mid-1;
+                high = mid-1;
             }
-            
+            else if( nums[mid] > target ){
+                high = mid-1;
+            }
+            else low = mid+1;
         }
         
-        if( first == -1 ) return {-1,-1};
-        
-        start = 0 ; 
-        end = nums.size()-1;
+        if ( first == -1) return {-1,-1};
+        // first position
+        low = 0 , high = nums.size()-1;
 
-        while( start <= end ){
-
-            int mid = start + ( end-start)/2;
-
-            if( nums[mid] > target ) end = mid-1;
-            else if( nums[mid] < target ) start = mid+1;
-            else {
+        int last = -1;
+        while( low <= high ){
+            
+            int mid = low + (high-low)/2;
+            if( nums[mid] == target){
                 last = mid;
-                start = mid+1;
+                low = mid+1;
             }
-
-        }   
+            else if( nums[mid] > target ){
+                high = mid-1;
+            }
+            else low = mid+1;
+        }
         
         return {first,last};
-        
     }
 };
