0c8ca230d7ae506efc019f751a2ff83d0e5c0893
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-02-12 06:01:51 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array (cpp)

--- solutiona/problems/search_in_rotated_sorted_array/solution.cpp
+++ solutiona/problems/search_in_rotated_sorted_array/solution.cpp
@@ -1,65 +1,24 @@
 class Solution {
 public:
-    
-    Solution(){
-        ios_base::sync_with_stdio(false);
-        cin.tie(0);
-        cout.tie(0);        
-    }
-
-    
-    
     int search(vector<int>& nums, int target) {
-        
-        if( nums.size() == 1 ) {
-            if( nums[0] == target ) return 0;
-            else return -1;
+        // search first index smaller than star
+        int low = 0 , high = nums.size()-1;
+        int pos = -1;
+        while( low <= high){
+            int mid = low + (high-low)/2;
+            if( nums[mid] >= nums[0] ) low = mid+1;
+            else pos = mid,high = mid-1;
         }
+        // cout<<pos;
         
-        int start = 1 , end = nums.size()-1, pos = 0;
+        if( pos == -1 ) low = 0 , high = nums.size()-1;        
+        else if( target >= nums[0] ) low = 0 , high = pos-1;
+        else low = pos , high = nums.size()-1;
         
-        while( start <= end ){
-            
-            int mid = start + (end-start)/2;
-            if( nums[0] < nums[mid] ){
-                start = mid+1;
-            }
-            else {
-                pos = mid;
-                end = mid-1;
-            }
-        }
-        
-        
-        
-        if ( pos == 0 ) {
-            start = 0 ; 
-            end = nums.size()-1;
-        }
-        else {
-            
-            if( target >= nums[pos] and target <= nums[nums.size()-1]){
-                start = pos ;
-                end = nums.size()-1;
-            }
-            else {
-                start = 0 ; 
-                end = pos - 1;
-            }
-            
-        }
-        
-        
-        
-        
-        while( start <= end ){
-            int mid = start + (end-start)/2;
-            if( nums[mid] > target ) {
-                end = mid-1;
-            }
-            else if( nums[mid] < target ){
-                start = mid+1;
-            }
+        while( low <= high ){
+            int mid = low + (high-low)/2;
+            if( nums[mid] > target) high = mid-1;
+            else if( nums[mid] < target) low = mid+1;
             else return mid;
         }
         
