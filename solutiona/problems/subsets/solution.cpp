01b681a0ffe7fa45a8a2efcf11ed5f757d4e4290
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-02-13 05:00:08 +0000

    Sync LeetCode submission - Subsets (cpp)

--- solutiona/problems/subsets/solution.cpp
+++ solutiona/problems/subsets/solution.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-	long long n = nums.size();
-	n = 1<<n;
-	vector<vector<int>> ret;
-	for( long long i = 0 ; i < n ; i++){
-		vector <int> temp;
-		for( long long j = 10 ; j >= 0 ; j--){
-			if( ((i>>j)&1) == 1 ) temp.push_back(nums[j]);
-		}
-		ret.push_back(temp);
-
-	}
-
-	return ret;        
+        vector <vector <int> > ret;
+        const int n = pow(2,nums.size());
+        for( int i = 0 ; i < n ; i++){
+            // loop through the binary 
+            // print 1 where it is 1
+            vector <int> temp;
+            for( int d = 31 ; d >= 0 ; d--){
+                int idx = ((i>>d)&1);
+                if( idx ) temp.push_back(nums[d]);
+            }
+            
+            ret.push_back(temp);
+        }
+        
+        return ret;
     }
 };
