12e6c2b57164d88e0d91312ed740570fe77c690e
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-02-13 05:08:40 +0000

    Sync LeetCode submission - Count Operations to Obtain Zero (cpp)

---
+++ solutiona/problems/count_operations_to_obtain_zero/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        int k = 0;
+        while( num1  and num2  ){
+            if( num1 > num2 )   num1 -= num2;
+            else    num2 -= num1;            
+            k++;
+        }        
+        return k;        
+    }
+};
