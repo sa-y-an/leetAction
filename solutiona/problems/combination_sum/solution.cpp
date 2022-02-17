6a66afcf447d8c65d40aea5d3ea8e26ba7865947
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-02-17 04:04:35 +0000

    Sync LeetCode submission - Combination Sum (cpp)

--- solutiona/problems/combination_sum/solution.cpp
+++ solutiona/problems/combination_sum/solution.cpp
@@ -1,36 +1,28 @@
 class Solution {
 public:
     
-    Solution(){
-        ios_base::sync_with_stdio(false);
-        cin.tie(0);
-        cout.tie(0);        
-    }
-
-    
-    vector < vector <int> > ret;
-    
-    void dfs( vector<int>& candidates, int target, int idx,vector <int> chosen = {}) {
+    void ifPossible(vector<vector<int>> &com, vector<int> &candidates, int target, int idx, vector <int> chosen = {} ){
         
-        if( target == 0 ){            
-            ret.push_back(chosen);
+        if(target == 0) {
+            com.push_back(chosen);
             return;
         }
-        if( target < 0 or idx < 0 ) return ;
-        
-        if(target-candidates[idx] >=0  ){
+      
+        if( target - candidates[idx] >= 0 ){
             chosen.push_back(candidates[idx]);
-            dfs(candidates,target-candidates[idx], idx , chosen);
-            chosen.pop_back();            
+            ifPossible(com,candidates,target-candidates[idx],idx,chosen);
+            chosen.pop_back();
         }
-
         
-        dfs(candidates,target, idx-1,chosen);
+        if( idx >= 1 ) {
+            ifPossible(com,candidates,target,idx-1,chosen);
+        }
+        
     }
     
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        
-        dfs(candidates, target, candidates.size()-1);        
+        vector <vector <int>> ret;
+        ifPossible(ret,candidates,target,candidates.size()-1);
         return ret;
     }
 };
