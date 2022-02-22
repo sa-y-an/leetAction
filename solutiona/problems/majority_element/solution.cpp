510179624383f4f1678fdf90278c22338ed1dd94
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-02-22 09:16:35 +0000

    Sync LeetCode submission - Majority Element (cpp)

--- solutiona/problems/majority_element/solution.cpp
+++ solutiona/problems/majority_element/solution.cpp
@@ -1,20 +1,12 @@
-
-int _IO =[](){
-    std :: ios :: sync_with_stdio (0);
-    cin.tie (0);
-    return 0;
- }();
-
 class Solution {
-public:  
+public:
     int majorityElement(vector<int>& nums) {
-        int cnt = 0, candidate = 0;     
-        for(auto num : nums ){
-            if(cnt == 0 ) candidate = num;
-            if( num == candidate ) cnt += 1;
-            else cnt -= 1;
-        }
-        
+        int candidate, credibility = 0;
+        for( auto num : nums ){
+            if( credibility == 0 ) candidate = num;
+            if( num == candidate ) credibility += 1;
+            else credibility -= 1;
+        }       
         return candidate;
     }
 };
