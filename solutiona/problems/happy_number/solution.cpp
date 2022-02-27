7315d28d52b5f15db22ae41b771b32bb8b79d6a9
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-02-27 17:53:53 +0000

    Sync LeetCode submission - Happy Number (cpp)

--- solutiona/problems/happy_number/solution.cpp
+++ solutiona/problems/happy_number/solution.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     
-    bool func(int n, unordered_set <int> & visited){
-        
-        if( n == 1 ) return true;
-        if( visited.find(n) != visited.end() ) return false;
-        
-        int sum = 0 ;
-        visited.insert(n);
-        while( n ){
-            int digit = n%10;
-            n/=10;
-            sum += digit*digit;
-        }
-        
-        
-        return func(sum,visited);
-    }
+    unordered_set <int> uset;
     
     bool isHappy(int n) {
-        unordered_set <int> visited;
-        return func(n,visited);
+        
+        while( n ){   
+            int sqSum = 0;
+            while( n ){
+                sqSum += (n%10)*(n%10);
+                n/= 10;
+            }
+            
+            if( sqSum == 1 ) return true;
+            n = sqSum;
+            
+            if( uset.find(n) != uset.end() ) return false;
+            uset.insert(n);
+        }
+        
+        return false;
+        
     }
 };
