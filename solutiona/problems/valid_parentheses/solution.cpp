529b6f2016c58a0b3033d9ef348697aa5afcf6bf
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-02-27 17:50:42 +0000

    Sync LeetCode submission - Valid Parentheses (cpp)

--- solutiona/problems/valid_parentheses/solution.cpp
+++ solutiona/problems/valid_parentheses/solution.cpp
@@ -1,40 +1,25 @@
 class Solution {
 public:
+    
+    unordered_map <char, int> umap = {
+        {'[',1}, {'(',2},{'{',3},
+        {']',-1}, {')',-2},{'}',-3},
+    };
     bool isValid(string s) {
         
-    unordered_map <char,int> umap = 
-    {
-        { '(' , -1},
-        { '{' , -2},
-        { '[' , -3},
-        { ')' ,  1},
-        { '}' ,  2},
-        { ']' ,  3},
+        stack <char> st;
         
-    };
-
-    stack <char> st;
-                 
-    for(auto val: s) {
-        
-        if(st.empty()) {
-            if(umap[val] > 0 ) return false;
-            st.push(val);            
-        }
-        else {
-            
-            if(umap[val] > 0 ) {
-                if(umap[st.top()] + umap[val] != 0 ) return false;
+        for( auto ch : s){
+            if( umap[ch] > 0 ){
+                st.push(ch);
+            }else{
+                if( st.empty()) return false;
+                if( umap[st.top()] + umap[ch] != 0 ) return false;
                 st.pop();
             }
-            else {
-                st.push(val);
-            }
-                       
+            
         }
-    }
         
-        if( st.empty() ) return true;
-        return false;   
+        return st.empty();
     }
 };
