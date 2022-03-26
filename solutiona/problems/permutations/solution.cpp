9acf5152a7581f411a335b35867807094390a964
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-03-26 13:19:16 +0000

    Sync LeetCode submission - Permutations (cpp)

--- solutiona/problems/permutations/solution.cpp
+++ solutiona/problems/permutations/solution.cpp
@@ -1,26 +1,27 @@
 class Solution {
+    vector <vector <int>> ans;
+     
 public:
-    vector<vector<int>> ans;
-    
-    void permutations(vector<int>& nums, vector <int> chosen= {}){
-        if( nums.size() == 0 ){
-            ans.push_back(chosen);         
+    void genPermute(vector <int> & nums, vector <int> chosen = {}){
+        
+        if( nums.size() == 0) {
+            ans.push_back(chosen);
             return;
         }
         
-        for( int i = 0 ; i < nums.size() ; i++){
-            int temp = nums[i];
+        for( int i = nums.size()-1 ; i >= 0 ; i--){
+            int poppedVal = nums[i];
+            chosen.push_back(nums[i]);
             nums.erase(nums.begin()+i);
-            chosen.push_back(temp);
-            permutations(nums,chosen);
+            genPermute(nums, chosen);
             chosen.pop_back();
-            nums.insert(nums.begin()+i,temp);
+            nums.insert(nums.begin()+i, poppedVal);
         }
         
     }
     
     vector<vector<int>> permute(vector<int>& nums) {
-        permutations(nums);
+        genPermute(nums);
         return ans;
     }
 };
