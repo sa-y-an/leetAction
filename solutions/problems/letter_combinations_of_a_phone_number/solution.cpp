dda06b8dbc4a3fa21204e03f6a2360a361d5ef83
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-03-28 07:36:43 +0000

    Sync LeetCode submission - Letter Combinations of a Phone Number (cpp)

--- solutions/problems/letter_combinations_of_a_phone_number/solution.cpp
+++ solutions/problems/letter_combinations_of_a_phone_number/solution.cpp
@@ -1,57 +1,24 @@
+vector <string> keyPad = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
 class Solution {
+    string chosen;
+    vector <string> ans;
 public:
-
-
-void findCombinations(string & str, vector <string> & letterCombinations, vector <string>& keypad, string combinations = ""){
-
-    if( str.empty()){
-        letterCombinations.push_back(combinations);
-        return;
-
-    }
-    else {
-        
-        int n = str.size()-1;
-        int digit = (int) (str[n] - '0' );
-        char temp = str[n];
-        str.pop_back();
-
-        for(int j = 0 ; j < keypad[ digit ].size() ; j++){
-            combinations.push_back( keypad[digit][j] );
-            findCombinations(str,letterCombinations,keypad,combinations);
-            combinations.pop_back();
+    
+    void combine(string &digits, int idx = 0){
+        if( idx == digits.size()) ans.push_back(chosen);
+        else {
+            int digit = digits[idx]-'0';
+            for( auto letter : keyPad[digit]){
+                chosen[idx] = letter;
+                combine(digits,idx+1);
+            }
         }
-
-        str.push_back(temp);
-
     }
-}
-
-        vector <string> letterCombinations(string digits){
-            
-            if( digits.empty()) return {};
-            
-            reverse(digits.begin(), digits.end());
-            
-            vector <string> combinations;
-            vector <string> keypad = {
-                "",
-                "",
-                "abc",
-                "def",
-                "ghi",
-                "jkl",
-                "mno",
-                "pqrs",
-                "tuv",
-                "wxyz"
-            };
-
-
-            findCombinations(digits, combinations, keypad);
-
-            return combinations;
-
-        }
-
+    
+    vector<string> letterCombinations(string &digits) {
+        if( digits.size() == 0 ) return {};
+        chosen.resize(digits.size());
+        combine(digits);
+        return ans;
+    }
 };
