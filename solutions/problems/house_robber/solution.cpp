420a40ae8fac32b2b4f3da252a8345c78c3f5e60
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-04-01 06:34:39 +0000

    Sync LeetCode submission - House Robber (cpp)

--- solutions/problems/house_robber/solution.cpp
+++ solutions/problems/house_robber/solution.cpp
@@ -1,24 +1,40 @@
+/* DP code
+int maxProfit(vector <int> &nums, int idx){
+
+    if( idx < 0 ) return 0;
+    if( dp[idx] != -1 ) return dp[idx];
+    int pick = nums[idx] + maxProfit(nums, idx-2);
+    int not_pick = maxProfit( nums, idx-1);
+
+    return dp[idx] = max(pick, not_pick);
+
+}
+*/
+// const int n = nums.size();
+// vector <int> dp(n,0);
+// dp[0] = nums[0];
+// for( int i = 1 ; i < n ; i++ ){
+//     int take = nums[i];
+//     if( i > 1 ) take += dp[i-2];
+//     int not_take = dp[i-1];
+//     dp[i] = max(take, not_take);
+// }
+
+// return dp[n-1];
+
 class Solution {
 public:
-    int rob(vector<int>& array) {
-    int n = array.size();
-	vector <int> maxSum(n,0);
-	if( not n ) return 0;	
-	maxSum[0] = array[0];
-	
-	if( n == 1 ) return array[0];
-	
-	maxSum[1] = array[1];
-	if( n == 2 ) return max(array[1], array[0]);
-	
-	maxSum[2] = array[2] + maxSum[0];
-	
-	if( n == 3 ) return max(maxSum[1], maxSum[2]);
-	
-	for( int i = 3 ; i < n ; i ++ )  {
-		maxSum[i] = max(maxSum[i-2],maxSum[i-3])+ array[i];
-	}
-	
-    return *max_element(maxSum.begin(), maxSum.end());
+    int rob(vector<int>& nums) {
+        const int n = nums.size();
+        int prev = nums[0], prev2 = 0;
+        for( int i = 1 ; i < n ; i++ ){
+            int take = nums[i];
+            if( i > 1 ) take += prev2;
+            int not_take = prev;
+            int curr = max(take, not_take);
+            prev2 = prev, prev = curr;
+        }
+        
+        return prev;
     }
 };
