05408e941e956c83655cfee81c661278c85aee5b
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-04-01 20:07:53 +0000

    Sync LeetCode submission - Minimum Path Sum (cpp)

--- solutions/problems/minimum_path_sum/solution.cpp
+++ solutions/problems/minimum_path_sum/solution.cpp
@@ -1,27 +1,32 @@
-class Solution {
-public:
-    Solution(){
-        ios_base::sync_with_stdio(false);
-        cin.tie(0);
-        cout.tie(0);        
-    }
+/*
+int minPath(vector <vector <int>> &grid, int x, int y ){
+    if( x < 0 or y < 0 ) return INT_MAX;
+    if( x == 0 and y == 0 ) return grid[0][0];
+    if( dp[y][x] != -1 ) return dp[y][x];
+    int ans = grid[y][x];
+    ans += min(minPath(grid, x-1, y), minPath(grid, x, y-1));
+    return dp[y][x] = ans;
 
-    
+}
+*/
+const int INF = 1e9+7;
+class Solution {
+    vector <vector <int>> dp;
+public:
     int minPathSum(vector<vector<int>>& grid) {
-        const unsigned short int X = grid[0].size(), Y = grid.size();
-        short int dp[Y][X];
-        dp[Y-1][X-1] = grid[Y-1][X-1];
-        
-        for( short int y = Y-2 ; y >= 0 ; y--) dp[y][X-1] = grid[y][X-1] + dp[y+1][X-1];
-        for( short int x = X-2 ; x >= 0 ; x--) dp[Y-1][x]= grid[Y-1][x] + dp[Y-1][x+1];
-        
-        for( short int y = Y-2 ; y >= 0 ; y--){
-            for( short int x = X-2; x >= 0 ; x--){
-                dp[y][x] = min(dp[y+1][x],dp[y][x+1]) + grid[y][x];
+        int n = grid.size() , m = grid[0].size();
+        vector <vector <int>> dp(n , vector <int> (m,INF));
+        dp[0][0] = grid[0][0];
+        for( int i = 0 ; i < n ; i++){
+            for( int j = 0 ; j < m ; j++){
+                dp[i][j] = grid[i][j];
+                int curr = INF;
+                i > 0 ? curr = min(curr, dp[i-1][j]) : curr;
+                j > 0 ? curr = min(curr, dp[i][j-1]) : curr;
+                curr < INF ? dp[i][j] += curr : curr;
             }
         }
         
-        return dp[0][0];
-        
+        return dp[n-1][m-1];
     }
 };
