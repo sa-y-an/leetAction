d042b07e7f421f3df80ad329c1dbdd1a9c1e230d
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-05-06 18:36:20 +0000

    Sync LeetCode submission - Remove Digit From Number to Maximize Result (cpp)

--- solutions/problems/remove_digit_from_number_to_maximize_result/solution.cpp
+++ solutions/problems/remove_digit_from_number_to_maximize_result/solution.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
-    string removeDigit(string number, char digit) {
-        
-        vector <string> nums;
-        for( int i = 0 ; i < number.size() ; i++){
-            if( number[i] == digit){
-                string temp;
-                for( int j = 0 ; j < number.size() ; j++){
-                    if( j == i ) continue;
-                    temp.push_back(number[j]);
-                }
-                nums.push_back(temp);
-            }
-        }
-        
-        sort( nums.begin(), nums.end());
-        return nums.back();
+    string removeDigit(string n, char digit) {
+        for (int i = 0; i < n.size() - 1; ++i)
+            if (n[i] == digit && n[i + 1] > digit)
+                return n.substr(0, i) + n.substr(i + 1);
+        int last_d = n.find_last_of(digit);
+        return n.substr(0, last_d) + n.substr(last_d + 1);
     }
 };
