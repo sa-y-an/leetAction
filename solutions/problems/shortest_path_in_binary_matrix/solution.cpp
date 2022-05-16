a11a566472902883bdefbdfd7b534ff2416f570a
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-05-16 16:06:17 +0000

    Sync LeetCode submission - Shortest Path in Binary Matrix (cpp)

--- solutions/problems/shortest_path_in_binary_matrix/solution.cpp
+++ solutions/problems/shortest_path_in_binary_matrix/solution.cpp
@@ -1,61 +1,29 @@
+const vector <pair<int, int>> movements = {{1,0},{-1,0},{0,1},{0,-1},{1,1},{-1,-1},{1,-1},{-1,1}};
 class Solution {
-    vector <pair <int, int>> movements;    
-public: 
-    Solution(){
-        ios_base::sync_with_stdio(false);
-        cin.tie(0);
-        cout.tie(0);        
-    }    
-    
-    struct children{
-        int x;
-        int y;
-        
-        children(int _x, int _y){
-            x = _x;
-            y = _y;
-        }
-        
-        bool isValid( vector<vector<int>>& grid, vector<vector<int>>& level ){
-            if( x >= grid[0].size() or y >= grid.size() or y < 0 or x < 0 ) return false;
-            if( level[y][x] != -1 ) return false;
-            if( grid[y][x] ) return false;
-            return true;
-        }
-    };
-    
-    void bfs(int x, int y, vector<vector<int>>& grid, vector<vector<int>>& level ){
-        
-        queue <children> q;
-        children parent(x,y);
-        q.push(parent);
-        level[y][x] = 1;
-        
-        while( not q.empty()){
-            
-            auto currVer = q.front();
-            q.pop();
-            
-            for( auto &move : movements ){
-                children child(currVer.x + move.first , 
-                               currVer.y + move.second);
-                if( not child.isValid(grid, level) ) continue;
-                level[child.y][child.x] = level[currVer.y][currVer.x] + 1;
-                q.push({child.x, child.y});
-             }
-            
-        }
-        
-    }
-    
+public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         
-        if( grid[0][0] or grid[grid.size()-1][grid.size()-1]) return -1;        
-        movements = {{1,0},{0,1},{-1,0},{0,-1},{1,1},{-1,-1},{-1,1},{1,-1}};
-        vector <vector <int>> level(grid.size(), vector <int> (grid[0].size(),-1) );
-        level[0][0] = 1;
-        bfs(0,0, grid, level);
-        return level[grid.size()-1][grid[0].size()-1];
+        int n = grid.size();
+        if( grid[0][0] == 1 or grid[n-1][n-1] == 1) return -1;
+        if( n == 1 ) return 1;
+        queue <pair <int, int>> q;
+        int level = 1;
+        q.push({0,0});
+        grid[0][0] = 1;
         
+        while( !q.empty()){
+            for( int sz = q.size(); sz-- ; q.pop()){
+                auto [y,x] = q.front();
+                if( x == n-1 and y == n-1 ) return level;
+                for( auto &[dy,dx] : movements ){
+                    int cy = y+dy, cx = x+dx;
+                    if( cy < 0 || cx < 0 || cx >= n || cy >= n || grid[cy][cx] == 1 ) continue;
+                    grid[cy][cx] = 1;
+                    q.push({cy,cx});
+                }
+            }
+            level++;
+        }
+        return -1;
     }
 };
