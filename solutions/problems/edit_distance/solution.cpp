9d14a6fa2dfc71b8d22d2c8260c1cc0f7524decd
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-05-17 19:26:39 +0000

    Sync LeetCode submission - Edit Distance (cpp)

--- solutions/problems/edit_distance/solution.cpp
+++ solutions/problems/edit_distance/solution.cpp
@@ -1,40 +1,18 @@
 class Solution {
 public:
-    Solution(){
-        ios_base::sync_with_stdio(false);
-        cin.tie(0);
-        cout.tie(0);        
-    }
-
-    
-    
-    int minDistance(string word1, string word2) {
+    int minDistance(string &word1, string &word2) {
+        int n = word1.size(), m = word2.size();
+        int dp[n+1][m+1];
+        memset(dp, 0, sizeof(dp));
+        for( int i = 0; i <= n ; i++) dp[i][0] = i;
+        for( int j = 0; j <= m ; j++) dp[0][j] = j;
         
-        vector <vector<int>> editDistance ( word1.size()+1 , vector <int> (word2.size()+1,0) );
-        const int ylim = word1.size()+1;
-        const int xlim = word2.size()+1;
-        
-        for( int y = ylim-1 ; y >= 0 ; y-- ){
-            editDistance[y][xlim-1] = ylim-y-1;
-        }
-        
-        for( int x = xlim - 1 ; x >= 0 ; x-- ){
-            editDistance[ylim-1][x] = xlim-x-1;
-        }
-        
-        for( int y = ylim-2; y >= 0 ; y-- ){
-            for( int x = xlim-2 ; x >= 0 ; x--  ){
-                if( word1[y] == word2[x] ){
-                    editDistance[y][x] = editDistance[y+1][x+1];
-                }
-                else {
-                    editDistance[y][x] = 1 + min( min(editDistance[y+1][x], editDistance[y][x+1]),
-                                                 editDistance[y+1][x+1]);
-                }
+        for( int i = 1; i <= n ; i++){
+            for( int j = 1 ; j <= m ; j++){
+                if( word1[i-1] == word2[j-1] ) dp[i][j] = dp[i-1][j-1];
+                else dp[i][j] = 1 + min({dp[i][j-1] ,dp[i-1][j], dp[i-1][j-1] });
             }
         }
-        
-                                                 
-      return editDistance[0][0];
+        return dp[n][m];
     }
 };
