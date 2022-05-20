b153662c8e4657daed295a405b0bd81d8ff8cf3a
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-05-20 03:36:20 +0000

    Sync LeetCode submission - Unique Paths II (cpp)

--- solutions/problems/unique_paths_ii/solution.cpp
+++ solutions/problems/unique_paths_ii/solution.cpp
@@ -1,29 +1,23 @@
-/*
-int numways(vector<vector<int>>& grid, int m, int n){
-    if( n == 0 and m == 0 ) return 1;
-    if( n < 0 or m < 0 or grid[n][m] == 1 ) return 0;
-    if( dp[n][m] != -1 ) return dp[n][m];
-    return dp[n][m] = numways(grid, m-1 , n ) + numways(grid, m , n-1);
-}
-*/
-
 class Solution {
+    vector <vector <int>> dp;
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
-        int n = grid.size() , m = grid[0].size();
-        if( grid[0][0] or grid[n-1][m-1]) return 0;
-        vector <vector <int>> dp(n , vector<int> (m,0));
-        dp[0][0] = 1;
+    
+    int dfs(vector<vector<int>>& obstacleGrid, int y, int x){
         
-        for( int i = 0 ; i < n ; i++){
-            for( int j = 0 ; j < m ; j++){
-                if( grid[i][j]) continue;
-                if( i > 0 ) dp[i][j] += dp[i-1][j];
-                if( j > 0 ) dp[i][j] += dp[i][j-1];
-            }
-        }
+        if( y < 0 or x < 0 or obstacleGrid[y][x] == 1 ) return 0;
+        if( y == 0 and x == 0 ) return dp[y][x] = 1;
+        if( dp[y][x] != -1 ) return dp[y][x];
         
-        return dp[n-1][m-1];
         
+        return dp[y][x] = dfs(obstacleGrid, y-1, x) + dfs(obstacleGrid, y, x-1);
+        
+    }
+    
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        
+        int n = obstacleGrid.size(), m = obstacleGrid[0].size();
+        if( obstacleGrid[0][0] == 1 || obstacleGrid[n-1][m-1] == 1) return 0;
+        dp.resize(n, vector <int> (m,-1));
+        return dfs(obstacleGrid,n-1, m-1);
     }
 };
