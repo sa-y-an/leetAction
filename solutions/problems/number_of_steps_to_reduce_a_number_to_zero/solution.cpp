a6fa0d46aec0993adf6d8a9f54eb2201df099ce1
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-05-27 04:59:38 +0000

    Sync LeetCode submission - Number of Steps to Reduce a Number to Zero (cpp)

--- solutions/problems/number_of_steps_to_reduce_a_number_to_zero/solution.cpp
+++ solutions/problems/number_of_steps_to_reduce_a_number_to_zero/solution.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     int numberOfSteps(int num) {
-        int k = 0;
-        while(num){
-            if( num%2 == 0 ) num/=2 , k++;
-            else num -= 1, k++;
-        }
-        return k;
+        return num ? __builtin_popcount(num) + 31 - __builtin_clz(num) : 0;
     }
 };
