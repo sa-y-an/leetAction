cd5f482c09d97640e28c434cec02a0124b086608
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-05-27 06:24:14 +0000

    Sync LeetCode submission - Hamming Distance (cpp)

--- solutions/problems/hamming_distance/solution.cpp
+++ solutions/problems/hamming_distance/solution.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     int hammingDistance(int x, int y) {
-        int ans = 0;
-        for(int i = 31 ; i >= 0 ; i--){
-            if( ((y>>i)&1)-((x>>i)&1) != 0 ) ans++;
-        }
-        return ans;
+        return __builtin_popcount(x^y);
     }
 };
