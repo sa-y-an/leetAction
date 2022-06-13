7f84a0b731075c6539418f44edcff54127c46ae3
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-06-13 01:30:00 +0000

    Sync LeetCode submission - Triangle (cpp)

--- solutions/problems/triangle/solution.cpp
+++ solutions/problems/triangle/solution.cpp
@@ -1,39 +1,21 @@
-/*
-int minSum(vector<vector<int>>& triangle, int idx = 0 , int prev = 0 ){
-
-    int n = triangle.size();
-    if( idx >= n ) return 0;
-    if( dp[idx][prev] != -1 ) return dp[idx][prev];
-
-    int ans = triangle[idx][prev] + minSum(triangle, idx+1 , prev);
-    if( prev+1 < triangle[idx].size() ) 
-        ans = min(triangle[idx][prev+1] + minSum(triangle,idx+1, prev+1), ans);
-
-    return dp[idx][prev] = ans;
-
-}
-*/
-const int INF = 1e5+7;
-
+const int INF = 1e7;
 class Solution {
+    vector <vector <int>> dp;
 public:
-    
+
     int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size(), m = triangle[n-1].size();
-        vector <vector <int>> dp(n, vector <int> (m,0));
+        
+        int n = triangle.size();
+        vector <int> prev(n+1,0);
+        vector <int> curr(n+1,0);
         
         for( int i = n-1 ; i >= 0 ; i--){
-            for( int j = m-1 ; j >= 0 ; j--){    
-                int first = INF, second = INF;
-                if( j < triangle[i].size()) first = triangle[i][j];
-                if( i+1 < n ) first += dp[i+1][j];
-                
-                if( j+1 < triangle[i].size() ) second = triangle[i][j+1];
-                if( i+1 < n and j+1 < m ) second += dp[i+1][j+1];
-                dp[i][j] = min(first, second);
+            int m = triangle[i].size();
+            for( int j = m-1 ; j >= 0 ; j--){
+                curr[j] = triangle[i][j] + min(prev[j+1], prev[j]);
             }
+            prev = curr;
         }
-        
-        return dp[0][0];
+        return curr[0];
     }
 };
