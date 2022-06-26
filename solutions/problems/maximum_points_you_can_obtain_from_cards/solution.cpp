f70ba986272c415a59a874deb2a191eb4ffe2cad
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-06-26 16:48:28 +0000

    Sync LeetCode submission - Maximum Points You Can Obtain from Cards (cpp)

--- solutions/problems/maximum_points_you_can_obtain_from_cards/solution.cpp
+++ solutions/problems/maximum_points_you_can_obtain_from_cards/solution.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
     int maxScore(vector<int>& cardPoints, int k) {
-        int l = k, r = cardPoints.size();
-        int ans = accumulate(cardPoints.begin(),cardPoints.begin()+l,0), 
-        curr = ans;
-        l--,r--;
-        while( l >= 0 ){
-            curr = curr - cardPoints[l] + cardPoints[r];
-            ans = max(curr,ans);
-            l--,r--;
+        
+        int n = cardPoints.size();
+        vector <int> prefix(n,0), suffix(n,0);
+        
+        prefix[0] = cardPoints[0];
+        for( int i = 1 ; i < n ; i++){
+            prefix[i] = cardPoints[i] + prefix[i-1];
         }
         
+        suffix[n-1] = cardPoints[n-1];
+        for( int i = n-2 ; i >= 0 ; i--){
+            suffix[i] = cardPoints[i] + suffix[i+1];
+        }
+        
+        int ans = 0;
+        for( int i = -1 ; i < k ; i++){
+            int p = i, s = i+ n-k +1 ;
+            int curr = 0;
+            if( p != -1 ) curr += prefix[p];
+            if( s != n ) curr += suffix[s];
+            ans = max(ans, curr);
+        }
         return ans;
     }
 };
