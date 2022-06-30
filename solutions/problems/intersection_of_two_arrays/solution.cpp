7c863e588befdf90a5c658a790a94f627a764d43
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-06-30 14:13:38 +0000

    Sync LeetCode submission - Intersection of Two Arrays (cpp)

--- solutions/problems/intersection_of_two_arrays/solution.cpp
+++ solutions/problems/intersection_of_two_arrays/solution.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set <int> uset(nums1.begin(),nums1.end());
-        unordered_set <int> uset2(nums2.begin(),nums2.end());
-        vector <int> ans;
-        for( int x : uset2) {
-            if(uset.count(x) > 0 ) ans.push_back(x);
+        set <int> st(nums1.begin(), nums1.end());
+        set <int> ans;
+        for( int x : nums2 ){
+            if( st.count(x) > 0 )
+                ans.insert(x);
         }
-        return ans;
+        return vector <int> (ans.begin(), ans.end());
     }
 };
