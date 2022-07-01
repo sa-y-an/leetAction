317d18642720fd2bf2c16e2fbe2a9b1afaa933a5
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-07-01 07:01:37 +0000

    Sync LeetCode submission - Maximum Units on a Truck (cpp)

--- solutions/problems/maximum_units_on_a_truck/solution.cpp
+++ solutions/problems/maximum_units_on_a_truck/solution.cpp
@@ -1,33 +1,21 @@
-bool comparator(vector <int> &a , vector <int> &b){
-    if( a[1] > b[1]) return true;
-    else return false;
+bool comparator( vector <int> & a, vector <int> &b){
+    return a[1] > b[1]; 
 }
 class Solution {
 public:
     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
-        
-        sort(boxTypes.begin(), boxTypes.end(), comparator);
-        int i = 0;
-        int leftOver = truckSize;
-        int ans=0;
-        int n = boxTypes.size();
-        while( leftOver > 0 and i < n ){
-            
-            if( leftOver - boxTypes[i][0] > 0 ) {
-                ans += boxTypes[i][1]*boxTypes[i][0];
-                leftOver -= boxTypes[i][0];
-                i++;
-            }
-            else {
-                ans += boxTypes[i][1]*leftOver;
-                leftOver = 0;
-                i++;
-            }
-            
+        sort( boxTypes.begin(), boxTypes.end(), comparator);
+        int k = truckSize, ans = 0;
+        for( auto &box : boxTypes ){
+            int n = box[0], u = box[1];
+            if( n <= k ){
+                ans += n*u;
+                k -= n;
+            }else {
+                ans += k*u;
+                break;
+            }   
         }
-        
-        
         return ans;
-        
     }
 };
