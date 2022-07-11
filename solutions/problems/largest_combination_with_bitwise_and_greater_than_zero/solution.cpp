7fd07e097f5e2bd7ee81ecf6865994f25c87c535
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-07-11 08:17:58 +0000

    Sync LeetCode submission - Largest Combination With Bitwise AND Greater Than Zero (cpp)

--- solutions/problems/largest_combination_with_bitwise_and_greater_than_zero/solution.cpp
+++ solutions/problems/largest_combination_with_bitwise_and_greater_than_zero/solution.cpp
@@ -1,16 +1,36 @@
 class Solution {
 public:
-    int largestCombination(vector<int>& candidates) {      
-        vector <int> arr(32,0);
-        int n = candidates.size();      
-        for (int i = 0; i < n; i++) {
-            int b = 31;
-            while (candidates[i] > 0) {
-                if (candidates[i] & 1 == 1) arr[b]++;
-                candidates[i] = candidates[i] >> 1;
-                b--;
-            }
+    
+    vector <int> i2b(int n){
+        
+        vector <int> ans(32,0);
+        for( int i = 31 ; i >= 0 ; i--){
+            if( ((n>>i)&1) )
+                ans[i] = 1;
+            else
+                ans[i] = 0;
         }
-        return *max_element(arr.begin(), arr.end());
+        return ans;
+    }
+    
+    int largestCombination(vector<int>& candidates) {
+        
+        vector <vector <int>> bin;
+        for( int i : candidates ){
+            bin.push_back(i2b(i));
+        }
+        
+        int n = candidates.size();
+        int maxi = 0;
+        
+        for( int x = 0 ; x < 32 ; x++){
+            int curr = 0;
+            for( int y = 0 ; y < n ; y++){
+                curr += bin[y][x];
+            }
+            maxi = max(curr, maxi);
+        }
+        
+        return maxi;
     }
 };
