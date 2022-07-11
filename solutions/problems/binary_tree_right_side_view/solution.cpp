b09f51220997e8b0fccad92ec2e369d8f0e8b104
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-07-11 07:44:07 +0000

    Sync LeetCode submission - Binary Tree Right Side View (cpp)

--- solutions/problems/binary_tree_right_side_view/solution.cpp
+++ solutions/problems/binary_tree_right_side_view/solution.cpp
@@ -9,47 +9,31 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-
 class Solution {
-    vector <vector<TreeNode*>> levelArr;
-    unordered_map <TreeNode*, int> level;
 public:
     vector<int> rightSideView(TreeNode* root) {
-        // right view is basicallly the last elemnt of a level
         
-        if ( not root ) return {};
-        
-        levelArr.resize(100);
-        queue <TreeNode*> q;
-        q.push(root);
-        level[root] = 0;
-        levelArr[level[root]].push_back(root);
-        
-        while( not q.empty()){
-            
-            auto curr = q.front();
-            q.pop();
-            
-            if( curr->left ){
-                q.push(curr->left);
-                level[curr->left] = level[curr]+1;
-                levelArr[level[curr->left]].push_back(curr->left);
-            }
-            if( curr->right){
-                q.push(curr->right);
-                level[curr->right] = level[curr]+1;
-                levelArr[level[curr->right]].push_back(curr->right);
-            }
-            
-        }
+        if( !root )
+            return {};
         
         vector <int> ans;
-        for( auto &lvl : levelArr){
-            if( lvl.size() == 0 ) break;
-            ans.push_back(lvl.back()->val);
+        queue <TreeNode*> q;
+        q.push(root);
+        
+        while( !q.empty()){
+            
+            TreeNode* curr;
+            int sz = q.size();
+            
+            for( ; sz-- ; q.pop()){
+                curr = q.front();
+                if( curr->left) q.push(curr->left);
+                if( curr->right) q.push(curr->right);
+            }
+            
+            ans.push_back(curr->val);
         }
         
-        return ans;
+        return ans;    
     }
 };
