a04a738585da8030ad37251d7c29c619ece2afec
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-07-15 07:37:20 +0000

    Sync LeetCode submission - Max Area of Island (cpp)

--- solutions/problems/max_area_of_island/solution.cpp
+++ solutions/problems/max_area_of_island/solution.cpp
@@ -1,31 +1,31 @@
-vector <pair<int, int> > movements = {{1,0},{0,1},{-1,0},{0,-1}};
-
+vector <pair <int,int>> mover = {{1,0},{0,1},{0,-1},{-1,0}};
 class Solution {
 public:
     
     
-    int dfs(vector <vector <int>> &grid, int y, int x){
+    int dfs(vector <vector <int>> &grid, int color, int y,int x){
         
         int n = grid.size(), m = grid[0].size();
-        if( y >= n || x >= m || y < 0 || x < 0 || grid[y][x] != 1 ) return 0;
-        grid[y][x] = 2;
+        grid[y][x] = color;
         int ans = 1;
-        for( auto &move : movements ){
-            ans += dfs(grid, y+move.second , x + move.first);
+        for( auto &[dy,dx] : mover ){
+            int i = y + dy , j = x + dx;
+            if( i >= 0 and i < n and j >= 0 and j < m and grid[i][j] == 1 ){
+                ans += dfs(grid,color,i,j);
+            }
         }
         
         return ans;
     }
     
-    
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-       
-        int n = grid.size(), m = grid[0].size(), ans = 0;
         
-        for( int i = 0 ; i < n ; i++ ){
+        int n = grid.size(), m = grid[0].size();
+        int ans = 0, color = 2 ;
+        for( int i = 0 ; i < n ; i++){
             for( int j = 0 ; j < m ; j++){
-                if( grid[i][j] == 1) 
-                    ans = max(dfs(grid, i, j), ans);
+                if( grid[i][j] == 1 )
+                    ans = max(ans, dfs(grid,color++,i,j));
             }
         }
         
