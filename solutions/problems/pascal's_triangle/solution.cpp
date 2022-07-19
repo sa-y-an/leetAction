b4dd012d558e8ff3f7ebba470cc94b5bcd41ac38
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-07-19 07:35:09 +0000

    Sync LeetCode submission - Pascal's Triangle (cpp)

--- solutions/problems/pascal's_triangle/solution.cpp
+++ solutions/problems/pascal's_triangle/solution.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) {
+    vector<vector<int>> generate(int n) {
         
-        vector < vector <int> > pascal;
-     
-        pascal.push_back({1});
-        
-        if( numRows == 1 ) return pascal;
-        
-        pascal.push_back({1,1});
-        
-        if( numRows == 2 ) return pascal;
-        
-        for( int row = 3 ; row < numRows+1 ; ++row ) {
-            
-            vector <int> temp(row, 1);
-            
-            for( int j = 0 ; j < row-2 ; j++ ){
-                temp[j+1] = (pascal[row-2][j] + pascal[row-2][j+1] ); 
+        vector <vector <int>> ans;
+        vector <int> prev(1,1);
+        ans.push_back(prev);
+        if( n == 1 )
+            return ans;
+        for( int i = 1 ; i < n ; i++){
+            vector <int> curr(i+1,1);
+            for( int j = 1; j < i ; j++){
+                curr[j] = ans[i-1][j] + ans[i-1][j-1];
             }
-            
-            pascal.push_back(temp);
-        } 
+            ans.push_back(curr);
+        }
         
-        return pascal;     
+        return ans;
     }
 };
