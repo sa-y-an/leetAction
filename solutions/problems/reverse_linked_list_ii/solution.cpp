9097517f0857f38ae98434090ab95771f2ff36bc
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-07-21 14:55:06 +0000

    Sync LeetCode submission - Reverse Linked List II (cpp)

--- solutions/problems/reverse_linked_list_ii/solution.cpp
+++ solutions/problems/reverse_linked_list_ii/solution.cpp
@@ -11,39 +11,53 @@
 class Solution {
 public:
     
+    ListNode* reverseLL(ListNode* head, ListNode* tail){
+        
+        if( !head || !head->next || head == tail )
+            return head;
+        
+        auto newHead = reverseLL(head->next, tail);
+        head->next->next = head;
+        return newHead;
+    }
+    
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        if( left == right ) return head;
-        ListNode* start, *end, *beforeStart, *afterEnd;
+        
+        if( !head->next )
+            return head;
+        
+        ListNode* first = nullptr, *second = nullptr, *prev = nullptr, *end = nullptr;
+        bool flag = true;
         
         int cnt = 1;
-        for( auto it = head ; it ; it = it->next , cnt++){
-            if( cnt == left -1 ) beforeStart = it;
-            else if( cnt == left ) start = it;
-            else if( cnt == right ) {
-                end = it;
-                break;
+        for(auto it = head ; it ; it = it->next , cnt++){
+            if( cnt == left ){
+                first = it;
+                flag = false;
+            }
+            if( cnt == right ){
+                second = it;
+            }
+            if( flag ){
+                prev = it;
             }
         }
-        afterEnd = end->next;
         
-        // reverse the part
-        auto prev = afterEnd;
-        auto it = start;
-        while( it != afterEnd ){
-            
-            auto tempNext = it->next;
-            it->next = prev;
-            prev = it;
-            
-            it = tempNext;
-        }
-        
-        if( beforeStart ) {
-            beforeStart->next = end;
+        if( first == nullptr or second == nullptr or first == second){
             return head;
         }
         
-        return end;
+        end = second->next;
+        if( left > 1){
+            prev->next = reverseLL(first,second);
+            first->next = end;
+        }
+        else {
+            prev = head;
+            auto prev2 = second->next;
+            head = reverseLL(first,second);
+            prev->next = prev2;
+        }
+        return head;
     }
-    
 };
