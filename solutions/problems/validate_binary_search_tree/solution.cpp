003718acc8046b1f54ccb356944fd56846f88521
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-08-11 18:35:31 +0000

    Sync LeetCode submission - Validate Binary Search Tree (cpp)

--- solutions/problems/validate_binary_search_tree/solution.cpp
+++ solutions/problems/validate_binary_search_tree/solution.cpp
@@ -9,43 +9,17 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+typedef long ll;
 class Solution {
 public:
     
-    queue <int> ret;
-    bool flag = true;    
-    
-    void inorderTraversal(TreeNode* root) {
-        
-        if( root == NULL ) return ;
-        
-        inorderTraversal(root->left);
-        
-        ret.push(root->val);
-        
-        if( ret.size() > 1 ) {
-            
-            if( ret.front() >= ret.back() ) {
-                flag = false;
-                return; 
-            }
-            ret.pop();
-
-        }
-     
-        
-        inorderTraversal(root->right);
-        
-    }
-    
-    bool isValidBST(TreeNode* root) {
-                
-        if( root->left || root->right ) {           
-            inorderTraversal(root);
-        }
-        
-        
-        
-        return flag;
+    bool isValidBST(TreeNode* root,ll upper = LONG_MAX, ll lower = LONG_MIN) {
+        if( !root )
+            return true;
+        if( root->val <= lower or root->val >= upper )
+            return false;
+        return isValidBST(root->left,root->val, lower) and 
+            isValidBST(root->right,upper,root->val); 
     }
 };
