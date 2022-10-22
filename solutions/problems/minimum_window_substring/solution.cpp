6aaba5b93e78e7e77f00cdcc9c9ab8b311725b88
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-10-22 16:38:47 +0000

    Sync LeetCode submission - Minimum Window Substring (cpp)

--- solutions/problems/minimum_window_substring/solution.cpp
+++ solutions/problems/minimum_window_substring/solution.cpp
@@ -1,42 +1,38 @@
+const int INF = 1e9+7;
 class Solution {
 public:
     
-    bool isMatch(vector <int> &window, vector <int> &target){
-        
-        for( int i = 0 ; i < 58; i++){
-            if( window[i] < target[i] )
+    bool isMatch(vector<int> &smap,vector <int> &tmap){
+        for( int i = 0 ; i < 60 ; i++){
+            if( smap[i] < tmap[i] )
                 return false;
         }
-        
         return true;
     }
     
     string minWindow(string s, string t) {
-        vector <int> window(58,0), target(58,0);
-        
-        for( char ch: t ) {
-            target[ch-'A']++;
+        vector <int> tmap(60,0) , smap(60,0);
+        for( char ch : t ) tmap[ch-'A']++;
+        const int n = s.size();
+        deque <int> q;
+        int mini = INF;
+        int start = -1;
+        for( int i = 0 ; i < n ; i++){
+            if( tmap[s[i]-'A'] == 0 ) continue;
+            smap[s[i]-'A']++;
+            q.push_back(i);
+            while( isMatch(smap, tmap) ){
+                int curr = q.back() - q.front() + 1;
+                if( curr < mini ){
+                    mini = curr;
+                    start = q.front();
+                }
+                smap[s[q.front()]-'A']--;
+                q.pop_front();
+            }
         }
         
-        int l = 0 , r = 0, mini = s.size()+1, start = -1, n = s.size() ;
-        
-        while( r < n ){
-            
-            while( r < n and !isMatch(window,target) ){
-                window[s[r++]-'A']++;
-            }
-            
-            while( l < r and isMatch(window,target)){
-                int len = r-l;
-                if( len < mini ) mini = len, start = l;
-                window[s[l++]-'A']--;
-            }
-            
-        }
-        
-        if( start == -1 ) return "";
-        string ans = s.substr(start, mini);
-        
-        return ans;
+        if( mini == INF ) return "";
+        return s.substr(start, mini);        
     }
 };
