62d920689506439a2cea3b39f7992e1bebb5a470
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-10-28 15:30:16 +0000

    Sync LeetCode submission - Group Anagrams (cpp)

--- solutions/problems/group_anagrams/solution.cpp
+++ solutions/problems/group_anagrams/solution.cpp
@@ -1,46 +1,18 @@
 class Solution {
 public:
-    Solution(){
-        ios_base::sync_with_stdio(false);
-        cin.tie(0);
-        cout.tie(0);        
-    }
-
-    
-    vector<vector<string>> groupAnagrams(vector<string>& words) {
-        
-        vector <pair <string,int>> wordIndex;
-        for( int i = 0 ; i < words.size() ; i++ ){
-            wordIndex.push_back({words[i],i});
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map <string, vector <string>> umap;
+        for( auto &str : strs ){
+            string copy = str;
+            sort(str.begin(), str.end());
+            umap[str].push_back(copy);
         }
-
-        for( auto & word: wordIndex ) {
-            sort(word.first.begin() , word.first.end());
+        
+        vector <vector<string>> ans;
+        for( auto &[k,v] : umap ){
+            ans.push_back(v);
         }
-
-        sort( wordIndex.begin(), wordIndex.end());
-
-        int i = 0 ;
-
-        vector < vector <string>> ret;
-
-        while( i < wordIndex.size() ) {
-            vector <string> temp;
-            temp.push_back( words[wordIndex[i].second]  );
-            i++;
-
-            while( i < wordIndex.size() and wordIndex[i-1].first == wordIndex[i].first ) {
-                temp.push_back(words[wordIndex[i].second] );
-                i++;
-            }
-
-            ret.push_back(temp);
-        }
-	
-	
-        return ret;
         
-        
-        
+        return ans;
     }
 };
