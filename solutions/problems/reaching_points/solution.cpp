c84d2980ebad55239403661e35a0a93e4e5b60f2
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-11-03 10:21:58 +0000

    Sync LeetCode submission - Reaching Points (cpp)

--- solutions/problems/reaching_points/solution.cpp
+++ solutions/problems/reaching_points/solution.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     bool reachingPoints(int sx, int sy, int tx, int ty) {
-        while( tx > sx and ty > sy ){
-            if( tx < ty ) ty %= tx;
+        
+        while( ty > sy and tx > sx ){
+            if( ty > tx ) ty %= tx;
             else tx %= ty;
         }
         
-        return ( sx == tx and sy <= ty and (ty-sy)%sx == 0 ) ||
-               ( sy == ty and sx <= tx and (tx-sx)%sy == 0);
+        return ( ty == sy and sx <= tx and (tx-sx)%ty == 0  ) or 
+            ( tx == sx and sy <= ty and (ty-sy)%tx == 0 );
     }
 };
